60b4e8aee5dd7f3cfde0072c94e5ef15ac7a2e1b
Mike Dickerson <inakajin@gmail.com>
2018-06-01 18:27:21 -0500

    cleaned up code

--- client/src/reducers/chat-data.js
+++ client/src/reducers/chat-data.js
@@ -1,82 +1,80 @@
 import {
-    FETCH_PROTECTED_DATA_SUCCESS,
-    FETCH_PROTECTED_DATA_ERROR
-} from '../actions/chat-data';
+  FETCH_PROTECTED_DATA_SUCCESS,
+  FETCH_PROTECTED_DATA_ERROR
+} from "../actions/chat-data";
 
 const initialState = {
-    data: '',
-    error: null,
-    rooms: [],
-    messages: [],
-    users: [],
+  data: "",
+  error: null,
+  rooms: [],
+  messages: [],
+  users: []
 };
 
 export default function reducer(state = initialState, action) {
- 
-    switch (action.type) {
-        case FETCH_PROTECTED_DATA_SUCCESS: {
-           
-            return {
-                ...state,
-                data: action.data,
-                error: null,
-                messages: action.data
-            }
-        }
-
-        case FETCH_PROTECTED_DATA_ERROR: {
-            return {
-                ...state,
-                error: action.error
-            }
-        }
-        case 'REALTIME_REFRESH': {
-            //console.log(action,state);
-            //if(action.payload.userId!=state.currentUser.id)
-           // {notifyMe(action.payload.message)}
-            let newMsg = [...state.messages]
-            newMsg.push({body: action.payload.message, username: action.payload.username, userId: action.payload.userId})
-            console.log(newMsg)
-            return {
-                ...state,
-                messages: newMsg,
-            }
-        }
-
-        case 'REALTIME_USERS': {
-            
-            let activeusers = state.users.map((user)=>{
-                if(action.payload.indexOf(user._id) > -1 ){
-                     user['active'] = true;
-                }
-                return user;
-             })
-            
-            return {
-                ...state,
-                users: activeusers
-            }
-        }
-
-        case 'FETCH_ROOMNAMES': {
-            console.log(action, state)
-            return {
-                ...state,
-                rooms: action.data
-            }
-        }
-
-        case 'FETCH_USERS': {
-            console.log(action, state)
-            return {
-                ...state,
-                users: action.data
-            }
-        }
-        default: 
-            return state;
+  switch (action.type) {
+    case FETCH_PROTECTED_DATA_SUCCESS: {
+      return {
+        ...state,
+        data: action.data,
+        error: null,
+        messages: action.data
+      };
     }
-    
+
+    case FETCH_PROTECTED_DATA_ERROR: {
+      return {
+        ...state,
+        error: action.error
+      };
+    }
+    case "REALTIME_REFRESH": {
+      //console.log(action,state);
+      //if(action.payload.userId!=state.currentUser.id)
+      // {notifyMe(action.payload.message)}
+      let newMsg = [...state.messages];
+      newMsg.push({
+        body: action.payload.message,
+        username: action.payload.username,
+        userId: action.payload.userId
+      });
+      console.log(newMsg);
+      return {
+        ...state,
+        messages: newMsg
+      };
+    }
+
+    case "REALTIME_USERS": {
+      let activeusers = state.users.map(user => {
+        if (action.payload.indexOf(user._id) > -1) {
+          user["active"] = true;
+        }
+        return user;
+      });
+
+      return {
+        ...state,
+        users: activeusers
+      };
+    }
+
+    case "FETCH_ROOMNAMES": {
+      return {
+        ...state,
+        rooms: action.data
+      };
+    }
+
+    case "FETCH_USERS": {
+      return {
+        ...state,
+        users: action.data
+      };
+    }
+    default:
+      return state;
+  }
 }
 
 /*function notifyMe(message) {
